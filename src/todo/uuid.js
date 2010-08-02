e9771c6e7eda0c032904917e1165f1f697d96eb7
cloudhead <self@cloudhead.net>
2010-08-02 03:05:02 -0400

    init uuid.js

---
+++ src/todo/uuid.js
@@ -0,0 +1,14 @@
+var db = require('../db').connection;
+
+var cache = [];
+
+this.generate = function (callback) {
+    if (cache.length > 0) {
+        callback(cache.pop());
+    } else {
+        db.uuids(100, function (err, data) {
+            Array.prototype.push.apply(cache, data);
+            callback(cache.pop());
+        });
+    }
+};
